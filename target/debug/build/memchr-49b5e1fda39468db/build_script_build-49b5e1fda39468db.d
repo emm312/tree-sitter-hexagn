783d94fde27f5058c0f5141f8c80b41a63513bb1
emm312 <emmtbm55@gmail.com>
2022-07-16 09:23:23 +1000

    initial commit

---
+++ target/debug/build/memchr-49b5e1fda39468db/build_script_build-49b5e1fda39468db.d
@@ -0,0 +1,5 @@
+c:\Users\emmtb\Desktop\coding\tree-sitter-hexagn\target\debug\build\memchr-49b5e1fda39468db\build_script_build-49b5e1fda39468db.exe: C:\Users\emmtb\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs
+
+c:\Users\emmtb\Desktop\coding\tree-sitter-hexagn\target\debug\build\memchr-49b5e1fda39468db\build_script_build-49b5e1fda39468db.d: C:\Users\emmtb\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs
+
+C:\Users\emmtb\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs:
---
+++ target/debug/build/tree-sitter-hexagn-60ef9535d7c9149b/build_script_build-60ef9535d7c9149b.d
@@ -0,0 +1,5 @@
+c:\Users\emmtb\Desktop\coding\tree-sitter-hexagn\target\debug\build\tree-sitter-hexagn-60ef9535d7c9149b\build_script_build-60ef9535d7c9149b.exe: bindings/rust/build.rs
+
+c:\Users\emmtb\Desktop\coding\tree-sitter-hexagn\target\debug\build\tree-sitter-hexagn-60ef9535d7c9149b\build_script_build-60ef9535d7c9149b.d: bindings/rust/build.rs
+
+bindings/rust/build.rs:
